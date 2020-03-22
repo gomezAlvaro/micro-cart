c2d521015642eceb4b930a9e21ea762decc03964
agomez <alvaro_melchor.gomez@lidl.es>
2020-03-22 09:31:35 +0100

    initial commit

---
+++ src/main/java/com/gomezr/microcart/MicroCartApplication.java
@@ -0,0 +1,13 @@
+package com.gomezr.microcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroCartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gomezr/microcart/MicroCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gomezr.microcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
